7fb033dd80e6bacd7423d4678215b77fb9aad551
alwynmathew <alwynmathew.90@gmail.com>
2023-06-15 10:56:48 +0100

    Added example for using session logger - removed session logger from other exmaples except with create API

--- examples/count_activity_tasks.py
+++ examples/count_activity_tasks.py
@@ -6,13 +6,17 @@
 
 import argparse
 import os
-import sys
 import time
 
-sys.path.insert(0, "../")
+try:
+    from DTP_config import DTPConfig
+except ModuleNotFoundError:
+    import sys
+
+    sys.path.insert(0, "../")
+    from DTP_config import DTPConfig
 
 from DTP_API import DTPApi
-from DTP_config import DTPConfig
 
 
 def parse_args():
@@ -23,7 +27,6 @@ def parse_args():
         description='Counts task nodes connected to a node identified by activity_node_iri')
     parser.add_argument('--xml_path', '-x', type=str, help='path to config xml file', required=True)
     parser.add_argument('--simulation', '-s', default=False, action='store_true')
-    parser.add_argument('--log_dir', '-l', type=str, help='path to log dir', required=True)
 
     return parser.parse_args()
 
@@ -32,9 +35,5 @@ if __name__ == "__main__":
     args = parse_args()
     dtp_config = DTPConfig(args.xml_path)
     dtp_api = DTPApi(dtp_config, simulation_mode=args.simulation)
-    if not os.path.exists(args.log_dir):
-        os.makedirs(args.log_dir)
-    log_path = os.path.join(args.log_dir, f"db_session-{time.strftime('%Y%m%d-%H%M%S')}.log")
-    dtp_api.init_logger(log_path)
     response = dtp_api.activity_count_connected_task_nodes("http://bim2twin.eu/mislata_wp3/activity91217940_2")
     print('Response:\n', response)
--- examples/use_session_logger.py
+++ examples/use_session_logger.py
@@ -26,6 +26,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Fetech all activity nodes from the platform')
     parser.add_argument('--xml_path', '-x', type=str, help='path to config xml file', required=True)
     parser.add_argument('--simulation', '-s', default=False, action='store_true')
+    parser.add_argument('--log_dir', '-l', type=str, help='path to log dir', required=True)
 
     return parser.parse_args()
 
@@ -34,5 +35,13 @@ if __name__ == "__main__":
     args = parse_args()
     dtp_config = DTPConfig(args.xml_path)
     dtp_api = DTPApi(dtp_config, simulation_mode=args.simulation)
+
+    # initialise session logger
+    if not os.path.exists(args.log_dir):
+        os.makedirs(args.log_dir)
+    log_path = os.path.join(args.log_dir, f"db_session-{time.strftime('%Y%m%d-%H%M%S')}.log")
+    dtp_api.init_logger(log_path)
+
+    # Session logger is not used in fetch APIs. This is for demonstration only.
     activities = dtp_api.query_all_pages(dtp_api.fetch_activity_nodes)
     print('Response:\n', activities)
