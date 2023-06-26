5f9c608b5c1e0b9b719865e6bbfef78c81612136
alwynmathew <alwynmathew.90@gmail.com>
2023-06-26 15:08:56 +0100

    Adapted node logging with lastest log updates

--- DTP_API.py
+++ DTP_API.py
@@ -106,11 +106,14 @@ class DTPApi(FetchAPI, CountAPI, CreateAPI, LinkAPI, RevertAPI, SendAPI, UpdateA
             self.log_markers = {**self.log_markers_node_classes, **other_log_markers}
 
         # initialise session logger
-        log_dir = os.path.join(self.DTP_CONFIG.get_log_path(), "sessions")
-        if not os.path.exists(log_dir):
-            os.makedirs(log_dir)
-        log_path = os.path.join(log_dir, f"db_session-{time.strftime('%Y%m%d-%H%M%S')}.log")
-        self.init_logger(log_path)
+        session_log_dir = os.path.join(self.DTP_CONFIG.get_log_path(), "sessions")
+        self.node_log_dir = os.path.join(self.DTP_CONFIG.get_log_path(), "nodes")
+        if not os.path.exists(session_log_dir):
+            os.makedirs(session_log_dir)
+        if not os.path.exists(self.node_log_dir):
+            os.makedirs(self.node_log_dir)
+        session_log_path = os.path.join(session_log_dir, f"db_session-{time.strftime('%Y%m%d-%H%M%S')}.log")
+        self.init_logger(session_log_path)
 
     def init_logger(self, session_file):
         """
