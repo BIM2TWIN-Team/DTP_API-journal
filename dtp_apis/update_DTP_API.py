2c1b5486820ed20d55cf546804f95dab537fa6ae
alwynmathew <alwynmathew.90@gmail.com>
2023-07-12 14:25:27 +0100

    Added activity name uri to DTP_config.xml

--- dtp_apis/update_DTP_API.py
+++ dtp_apis/update_DTP_API.py
@@ -100,7 +100,7 @@ class UpdateAPI:
         with open(dump_path, 'w') as fp:
             json.dump(node_info, fp)
 
-        out_edge_dict = []
+        out_edge_dict = {}
         if target_as_built_iri:
             out_edge_dict = {
                 "_label": self.DTP_CONFIG.get_ontology_uri('hasAction'),
@@ -110,7 +110,7 @@ class UpdateAPI:
         query_dict = {
             "_domain": self.DTP_CONFIG.get_domain(),
             "_iri": action_node_iri,
-            "_outE": out_edge_dict
+            "_outE": [out_edge_dict]
         }
 
         if contractor:
